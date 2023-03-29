668c29df90650cdcfff66118793a311dd2aeeb13
Nilkanth-Patadiya <nilkanthpatadiya1@gmail.com>
2023-03-29 22:22:32 +0530

    Update server to handle exit gracefully

--- app.js
+++ app.js
@@ -1,22 +1,20 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-var cors = require('cors')
+var express = require("express")
+var path = require("path")
+var cookieParser = require("cookie-parser")
+var logger = require("morgan")
+var cors = require("cors")
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var indexRouter = require("./routes/index")
 
-var app = express();
+var app = express()
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
+app.use(logger("dev"))
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+app.use(cookieParser())
 app.use(cors())
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")))
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use("/", indexRouter)
 
-module.exports = app;
+module.exports = app
